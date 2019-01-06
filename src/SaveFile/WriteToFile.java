b022de910fa610fe0b396af4f6cccc72ae9d6c5f
tolstopus <sergio.glock@gmail.com>
2019-01-06 19:46:59 +0200

    write to file

---
+++ src/SaveFile/WriteToFile.java
@@ -0,0 +1,17 @@
+package SaveFile;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class WriteToFile {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("test.txt");
+        PrintWriter pw = new PrintWriter(file);
+
+        pw.println("Test row 1");
+        pw.println("Test row 2");
+
+        pw.close();
+    }
+}
